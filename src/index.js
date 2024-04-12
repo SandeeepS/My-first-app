08fb26ffa221e1aa6e00944e17835a96d705c6fa
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-04-12 15:34:16 +0530

    sample index file changed

--- src/index.js
+++ src/index.js
@@ -4,11 +4,51 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function ListItem(){
+    return (
+      <div className='list-item'>
+      <hr />
+      <div className='list-title'>
+          <h4>My Tittle One</h4>
+      </div>
+      <div className='list-dis'>
+          This is a very big description
+      </div>
+      <div className='list-label'>
+              <span>label1</span>
+              <span>label2</span>
+              <span>label3</span>
+      </div>
+      </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function myFunction(){
+  console.log("clicked");
+}
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode> 
+  <div className='app'>
+     <div className='app-header'>
+          <span className='header-tittle'>Web development</span>
+          <span>Home</span>
+          <span>Usage</span>
+          <span>Settings</span>
+          <span>Logout</span>
+     </div>
+     <div className='app-body'>
+          <div className='app-list'>
+              <ListItem />
+              <ListItem />
+              <ListItem />
+              <ListItem />
+          </div>
+     </div>
+     <div className='app-root'>
+            Copyright.in 
+     </div>
+  </div>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
